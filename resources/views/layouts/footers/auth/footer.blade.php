58f7e9ad196a37eb9f9f31dfdb20c550451b5bf1
einteriano1997 <interiano.edwin.2014@gmail.com>
2024-08-21 12:52:28 -0600

    Cambios en proyecto

---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,29 @@
+<div class="container text-center" style="background-color: #d32f2f; padding: 50px 0;">
+    <h2 class="text-white">Sé Parte del Programa Sabios & Expertos</h2>
+    <div class="row mt-5">
+        <!-- PYMES Section -->
+        <div class="col-md-6">
+            <div class="mb-4">
+                <img src="https://img.icons8.com/ios-filled/100/ffffff/company.png" alt="PYMES Icon">
+            </div>
+            <h4 class="text-white">PYMES</h4>
+            <p class="text-white">
+                Si eres una pequeña o mediana empresa puedes solicitar una asesoría empresarial de parte de un experto retirado.
+                Llena el formulario de aplicación a continuación.
+            </p>
+            <button class="btn btn-light mt-3">APLICAR</button>
+        </div>
+        <!-- Estudiantes Universitarios Section -->
+        <div class="col-md-6">
+            <div class="mb-4">
+                <img src="https://img.icons8.com/ios-filled/100/ffffff/student-male.png" alt="Estudiantes Universitarios Icon">
+            </div>
+            <h4 class="text-white">Estudiantes universitarios</h4>
+            <p class="text-white">
+                Si eres un estudiante universitario de cuarto o quinto año puedes llenar nuestro formulario para participar
+                en las diferentes oportunidades de internato que ofrece el Programa Sabios & Expertos.
+            </p>
+            <button class="btn btn-light mt-3">APLICAR</button>
+        </div>
+    </div>
+</div>
